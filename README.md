cbdbda9a3cc83a3aeb36e9073c888b2851f073cb
Rocio <32850111+rochimo2@users.noreply.github.com>
2025-06-05 15:16:42 -0300

    Agregue opcion para que el usuario decida cuantos turnos quiere tomar y complete el readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Trabajo Integrador Programación I 
+### ALumnas Belén Grosso y Rocío moyano
 
 Este repositorio contiene un trabajo integrador desarrollado en Python como parte de la materia Programación I. El objetivo principal del proyecto es desarrollar el contenido de Busqueda y Ordenamiento, integrando los distintos conceptos y las herramientas para su uso. Tambien se incorpora un archivo .py donde se desarrolla el caso práctico.
+
+
+## Instrucciones para correr el código
+
+En la consola, ingrese el nombre del archivo trabajo_integrador.py para ejecutarlo.
+Complete su nombre y apellido, mail y especialidad a buscar cuando se lo requirea.
+Si desea tomar un nuevo turno ingrese si, de lo contrario ingrese no y el programa finalizará.
--- Trabajo_Integrador.py
+++ Trabajo_Integrador.py
@@ -66,49 +66,53 @@ especialistas_codigos = {
 
 
 numeros = [especialistas_codigos[x] for x in lista_especialistas_ordenada]
-# print(numeros) [Numeros desordenados]
 
+repetir = True
 
 # Aplicacion de quicksort y busqueda binaria
+while repetir:
 
-nombre = (input("¡Bienvenido/a! Por favor ingrese su nombre y apellido: "))
-mail = (input("Por favor ingrese su mail: "))
-print("------------------------------")
+    nombre = (input("¡Bienvenido/a! Por favor ingrese su nombre y apellido: "))
+    mail = (input("Por favor ingrese su mail: "))
+    print("------------------------------")
 
-print(f"Estimado/a paciente {nombre}, a continuación se mostrarán los especialistas médicos disponibles hasta la fecha en orden descendente.")
-print("Cada numero quivale a la especializacion disponible en nuestro sistema. Por ejemplo, el número **4** equivaldrá a la cuarta especialidad en la lista de especialistas médicos")
-print("Menor a 5: equivale a las especialidades con menor disponibilidad hasta el momento.")
-print()
-print("===== Disponibilidad =====")
+    print(f"Estimado/a paciente {nombre}, a continuación se mostrarán los especialistas médicos disponibles hasta la fecha en orden descendente.")
+    print("Cada numero quivale a la especializacion disponible en nuestro sistema. Por ejemplo, el número **4** equivaldrá a la cuarta especialidad en la lista de especialistas médicos")
+    print("Menor a 5: equivale a las especialidades con menor disponibilidad hasta el momento.")
+    print()
+    print("===== Disponibilidad =====")
 
-print(quicksort(numeros)) # traemos la lista de numeros utilizando quicksort
+    print(quicksort(numeros)) # traemos la lista de numeros utilizando quicksort
 
-print()
-print("===== Especialistas Médicos =====")
-print(quicksort(lista_especialistas_ordenada)) # traemos la lista de especialidades ya ordenada utilizando quicksort
+    print()
+    print("===== Especialistas Médicos =====")
+    print(quicksort(lista_especialistas_ordenada)) # traemos la lista de especialidades ya ordenada utilizando quicksort
 
 
-print()
-busqueda = input("Ingrese la especialidad que desea buscar (ej. Pediatra): ").capitalize()
+    print()
+    busqueda = input("Ingrese la especialidad que desea buscar (ej. Pediatra): ").capitalize()
 
-# Realizar la búsqueda
-indice = busqueda_binaria(lista_especialistas_ordenada, busqueda)
+    # Realizar la búsqueda
+    indice = busqueda_binaria(lista_especialistas_ordenada, busqueda)
 
-# El usuario al escribir la profesion, se marca la posicion dentro de la lista.
+    # El usuario al escribir la profesion, se marca la posicion dentro de la lista.
 
-if indice != -1 and busqueda in doctores_por_especialidad:
-    print(f"\nEspecialidad encontrada en la posición {indice}.")
+    if indice != -1 and busqueda in doctores_por_especialidad:
+        print(f"\nEspecialidad encontrada en la posición {indice}.")
 
-# Mensaje aclarando que si la posición es 5 o mayor saldra un mensaje marcando menor disponibilidad
-    if indice >= 5:
-        print("Atención: Esta especialidad presenta menor disponibilidad. Se recomienda consultar turnos con anticipación a clinica@medicos.com.")
+    # Mensaje aclarando que si la posición es 5 o mayor saldra un mensaje marcando menor disponibilidad
+        if indice >= 5:
+            print("Atención: Esta especialidad presenta menor disponibilidad. Se recomienda consultar turnos con anticipación a clinica@medicos.com.")
 
-    print("\nDoctores disponibles:")
-    for doctor in doctores_por_especialidad[busqueda]:
-        print(f"- {doctor}")
-else:
-# Si no coindice con el contenido de la lista
-    print("Especialidad no encontrada.")
+        print("\nDoctores disponibles:")
+        for doctor in doctores_por_especialidad[busqueda]:
+            print(f"- {doctor}")
+    else:
+    # Si no coindice con el contenido de la lista
+        print("Especialidad no encontrada.")
 
-print()
-print(f"Le llegara un boletin informativo una vez asignado el turno a este correo: {mail}. ¡Muchas gracias!")
+    print()
+    print(f"Le llegara un boletin informativo una vez asignado el turno a este correo: {mail}. ¡Muchas gracias!")
+    
+    otro_turno = (input("¿Desea tomar otro turno? Ingrese Si o No: "))
+    repetir = True if otro_turno.capitalize() == 'Si' else False
